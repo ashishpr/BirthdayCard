9b6ae0ada28afc8529e062b3a4ef207f9931c442
Ashish <ashishprakash93@gmail.com>
2019-09-01 19:58:10 +0530

    Simple Birthday Card

---
+++ app/src/main/java/com/example/cake/LearnLayout.java
@@ -0,0 +1,14 @@
+package com.example.cake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_layout);
+    }
+}
